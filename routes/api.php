7a1b9793c2270f91aa36c0044a07444c98f638c9
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-04-30 10:02:28 +0700

    Some Complex Transaction Method

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ use Illuminate\Http\Request;
 
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
 
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
+
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
 
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
@@ -27,4 +29,9 @@ Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show
 
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
 
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
+
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
+
