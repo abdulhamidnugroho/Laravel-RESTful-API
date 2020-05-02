a0edb2da46fe551e0327431dbb85577964bf20bd
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-05-02 09:45:25 +0700

    ++ ProductCategory for Index

--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,8 @@ Route::resource('products.transactions', 'Product\ProductTransactionController',
 
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
 
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
+
 
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 
