5a2d87279af51283c5e80296e86153e67458d7a4
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-05-02 09:06:29 +0700

    (Final) ++ Destroy method for SellerProduct

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -114,9 +114,13 @@ class SellerProductController extends Controller
      * @param  \App\Seller  $seller
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Seller $seller)
+    public function destroy(Seller $seller, Product $product)
     {
-        //
+        $this->checkSeller($seller, $product);
+
+        $product->delete();
+
+        return $this->showOne($product);
     }
 
     protected function checkSeller(Seller $seller, Product $product)
