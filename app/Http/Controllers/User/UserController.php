7d5a58bdde8aee836a66a99ab914fc824c1f9489
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-05-06 14:34:28 +0700

    Resend Email Verification

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\User;
 
 use App\Traits\ApiResponser;
 use App\Http\Controllers\Controller;
+use App\Mail\UserCreated;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -159,5 +161,17 @@ class UserController extends Controller
         $user->save();
 
         return $this->showMessage('The account has been verified successfully');
+    }
+
+    public function resend(User $user)
+    {
+        if ($user->isVerified())
+        {
+            return $this->errorResponse('This user is already verified', 409);
+        }
+
+        Mail::to($user)->send(new UserCreated($user));
+
+        return $this->showMessage('The verification email has been resend');
     }
 }
