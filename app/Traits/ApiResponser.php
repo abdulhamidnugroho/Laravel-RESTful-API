99519f477d1c7e74cb8cd643a2a8f9682580e5db
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-05-08 22:17:46 +0700

    ++ allowing custom per page size

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -8,6 +8,7 @@ use Spatie\Fractal\Fractal;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -81,9 +82,19 @@ trait ApiResponser
 
     protected function paginate(Collection $collection)
     {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
         $page = LengthAwarePaginator::resolveCurrentPage();
 
         $perPage = 15;
+        if (request()->has('per_page'))
+        {
+            $perPage = (int)request()->per_page;
+        }
 
         $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
 
