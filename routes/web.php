f635cd5fa35d59a1797dad6b615ca324a7662eb7
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-05-11 12:29:51 +0700

    Fix routing (laravel version)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,32 +12,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Auth::routes();
 
 // Authentication Routes...
-$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-$this->post('login', 'Auth\LoginController@login');
-$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
 if ($options['register'] ?? true) {
-    $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-    $this->post('register', 'Auth\RegisterController@register');
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
 }
 
 // Password Reset Routes...
 if ($options['reset'] ?? true) {
-    $this->resetPassword();
+    Route::resetPassword();
 }
 
 // Password Confirmation Routes...
 if ($options['confirm'] ??
-    class_exists($this->prependGroupNamespace('Auth\ConfirmPasswordController'))) {
-    $this->confirmPassword();
+    class_exists(Route::prependGroupNamespace('Auth\ConfirmPasswordController'))) {
+    Route::confirmPassword();
 }
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('guest');
