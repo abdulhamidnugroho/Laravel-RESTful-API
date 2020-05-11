3545a86594b24070088e7ba2c1cc054830142bbd
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-05-11 11:10:01 +0700

    modifiy the routing

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
+
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+if ($options['register'] ?? true) {
+    $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    $this->post('register', 'Auth\RegisterController@register');
+}
+
+// Password Reset Routes...
+if ($options['reset'] ?? true) {
+    $this->resetPassword();
+}
+
+// Password Confirmation Routes...
+if ($options['confirm'] ??
+    class_exists($this->prependGroupNamespace('Auth\ConfirmPasswordController'))) {
+    $this->confirmPassword();
+}
 
 Route::get('/home', 'HomeController@index')->name('home');
